cbbb5b11358dc54638a3ad9a4a6d2859a54ba5e8
marbleville <allen.ehrhardt@gmail.com>
2023-11-10 11:08:51 -0500

    setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+fs.createReadStream("./inventory.csv")
+	.pipe(parse({ delimiter: ",", from_line: 2 }))
+	.on("data", function (row) {
+		console.log(row);
+	})
+	.on("error", function (error) {
+		console.log(error.message);
+	})
+	.on("end", function () {
+		console.log("finished");
+	});
