f889e52d423590004af3fe062cfde27fad7e35a1
marbleville <allen.ehrhardt@gmail.com>
2023-11-10 16:09:36 -0500

    code runs but over counting

--- index.js
+++ index.js
@@ -11,26 +11,27 @@ let productObj = {
 };
 
 let sortedProducts = [];
+let linesProcessed = 0;
 
 fs.createReadStream("./inventory.csv")
 	.pipe(parse({ delimiter: ",", from_line: 2 }))
 	.on("data", function (row) {
+		linesProcessed++;
 		let o = Object.create(productObj);
 		o.id = row[0];
 		o.name = row[1];
 		o.category = row[2];
 		o.brandId = row[3];
 		o.quantitySold = getTotalSold(row);
-		insertProduct(o);
+		sortedProducts = insertProduct(o, sortedProducts);
+		console.log(`${linesProcessed} lines processed`);
 	})
 	.on("error", function (error) {
 		console.log(error.message);
 	})
 	.on("end", function () {
 		for (let i = 0; i < topNumber; i++) {
-			console.log(
-				`Product: ${sortedProducts[i].name} - Quantity Sold: ${sortedProducts[i].quantitySold}`
-			);
+			console.log(sortedProducts[i]);
 		}
 	});
 
@@ -39,28 +40,37 @@ function getTotalSold(inventory) {
 
 	// Start at 5 because the first 4 rows are the headers and I want to compare backwards starting at the second value
 	for (let i = 5; i < inventory.length; i++) {
-		let delta = 0;
-		if (inventory[i - 1] != "" && inventory[i] != "") {
-			delta = parseInt(inventory[i - 1]) - parseInt(inventory[i]);
-		}
+		let prev = parseInt(inventory[i - 1]);
+		let cur = parseInt(inventory[i]);
 
-		// Positive delta means the inventroy decreased -> items sold
-		if (delta > 0) {
-			total += delta;
+		if (prev != NaN && cur != NaN) {
+			let delta = prev - cur;
+
+			// Positive delta means the inventory decreased -> items sold
+			if (delta > 0) {
+				total += delta;
+			}
 		}
 	}
-
 	return total;
 }
 
-function insertProduct(product) {
-	let index = 0;
-	for (let i = 0; i < sortedProducts.length; i++) {
-		if (product.quantitySold > sortedProducts[i].quantitySold) {
-			index = i;
-			break;
-		}
+// Inserts a product into the sortedProducts array maintaining the descending order
+function insertProduct(product, sp) {
+	if (sp.length === 0) {
+		return [product];
 	}
 
-	sortedProducts.splice(index, 0, product);
+	let sorted = [];
+	for (let i = 0; i < sp.length; i++) {
+		if (sp[i].quantitySold > product.quantitySold) {
+			sorted.push(sp[i]);
+		} else {
+			let rest = sp.slice(i);
+			sorted.push(product);
+			sorted = sorted.concat(rest);
+			return sorted;
+		}
+	}
+	return sorted;
 }
